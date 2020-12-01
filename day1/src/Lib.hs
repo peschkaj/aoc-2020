1c74a9ec7338a89c39814782274dc017080004d4
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2020-12-01 21:16:34 +0000

    Clean up code formatting

--- day1/src/Lib.hs
+++ day1/src/Lib.hs
@@ -1,37 +1,26 @@
 module Lib
     ( readIntCode
     , find2020
-    , find2020'
     ) where
 
 readIntCode :: String -> IO [Int]
 readIntCode filename = wordsToInts . words <$> readFile filename
   where wordsToInts = map read
 
-sublistofsize :: Int -> [a] -> [[a]]
-sublistofsize n xs = sublistofsize' n xs [] []
-  where
-    sublistofsize' 0 _        prefix otherResults = reverse prefix : otherResults
-    sublistofsize' _ []       _      otherResults = otherResults
-    sublistofsize' n (x : xs) prefix otherResults = sublistofsize' (n-1) xs (x:prefix) (sublistofsize' n xs prefix otherResults)
-
 combinationsOf :: Int -> [a] -> [[a]]
-combinationsOf 1 as        = map pure as
-combinationsOf k as@(x:xs) = run (l-1) (k-1) as $ combinationsOf (k-1) xs
-                             where
-                             l = length as
+combinationsOf 1 as = map pure as
+combinationsOf k as@(x : xs) = run (l -1) (k -1) as $ combinationsOf (k -1) xs
+  where
+    l = length as
 
-                             run :: Int -> Int -> [a] -> [[a]] -> [[a]]
-                             run n k ys cs | n == k    = map (ys ++) cs
-                                           | otherwise = map (q:) cs ++ run (n-1) k qs (drop dc cs)
-                                           where
-                                           (q:qs) = take (n-k+1) ys
-                                           dc     = product [(n-k+1)..(n-1)] `div` product [1..(k-1)]
+    run :: Int -> Int -> [a] -> [[a]] -> [[a]]
+    run n k ys cs
+      | n == k = map (ys ++) cs
+      | otherwise = map (q :) cs ++ run (n -1) k qs (drop dc cs)
+      where
+        (q : qs) = take (n - k + 1) ys
+        dc = product [(n - k + 1) .. (n -1)] `div` product [1 .. (k -1)]
 
 find2020 :: Int -> [Int] -> [Int]
-find2020 n xs = head $ take 1 $ filter eq2020 $ sublistofsize n xs
-  where eq2020 p = sum p == 2020
-
-find2020' :: Int -> [Int] -> [Int]
-find2020' n xs = head $ take 1 $ filter eq2020 $ combinationsOf n xs
+find2020 n xs = head $ take 1 $ filter eq2020 $ combinationsOf n xs
   where eq2020 p = sum p == 2020
