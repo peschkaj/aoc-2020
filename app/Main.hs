984b37c71981a35d32116a0d67416af368eebd6c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2020-12-03 12:44:18 +0000

    stuff and things

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import qualified Day1 as Day1
 import qualified Day2 as Day2
+import qualified Day3 as Day3
 
 main :: IO ()
 main = do
@@ -15,7 +16,7 @@ main = do
     -}
 
   -- day 2
-  p1 <- Day2.part1 "C:\\Users\\jeremiah\\src\\peschkaj\\aoc2020\\day2\\input.txt"
+  p1 <- Day2.part1 "C:\\Users\\jeremiah\\src\\peschkaj\\aoc2020\\day2.txt"
   print p1
-  p2 <- Day2.part2 "C:\\Users\\jeremiah\\src\\peschkaj\\aoc2020\\day2\\input.txt"
+  p2 <- Day2.part2 "C:\\Users\\jeremiah\\src\\peschkaj\\aoc2020\\day2.txt"
   print p2
